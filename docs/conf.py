5c881b3aa8f9b6b9300e163986d72dea21bb3140
RuiqiaoHe <34981680+RuiqiaoHe@users.noreply.github.com>
2024-12-11 16:18:34 +0800

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -53,12 +53,6 @@ templates_path = ['_templates']
 exclude_patterns = []
 
 ###
-nbsphinx_thumbnails = {
-    'gallery/thumbnail-from-conf-py': 'gallery/a-local-file.png',
-    'gallery/*-rst': 'images/notebook_icon.png',
-    'orphan': '_static/favicon.svg',
-}
-
 # This is processed by Jinja2 and inserted before each notebook
 nbsphinx_prolog = r"""
 {% set docname = 'doc/' + env.doc2path(env.docname, base=None)|string %}
@@ -66,11 +60,6 @@ nbsphinx_prolog = r"""
 .. raw:: html
 
     <div class="admonition note">
-      This page was generated from
-      <a class="reference external" href="https://github.com/spatialaudio/nbsphinx/blob/{{ env.config.release|e }}/{{ docname|e }}">{{ docname|e }}</a>.
-      Interactive online version:
-      <span style="white-space: nowrap;"><a href="https://mybinder.org/v2/gh/spatialaudio/nbsphinx/{{ env.config.release|e }}?filepath={{ docname|e }}"><img alt="Binder badge" src="https://mybinder.org/badge_logo.svg" style="vertical-align:text-bottom"></a>.</span>
-      <a href="{{ env.docname.split('/')|last|e + '.ipynb' }}" class="reference download internal" download>Download notebook</a>.
       <script>
         if (document.location.host) {
           let nbviewer_link = document.createElement('a');
@@ -87,66 +76,8 @@ nbsphinx_prolog = r"""
         }
       </script>
     </div>
-
-.. raw:: latex
-
-    \nbsphinxstartnotebook{\scriptsize\noindent\strut
-    \textcolor{gray}{The following section was generated from
-    \sphinxcode{\sphinxupquote{\strut {{ docname | escape_latex }}}} \dotfill}}
+    
 """
-
-# This is processed by Jinja2 and inserted after each notebook
-nbsphinx_epilog = r"""
-{% set docname = 'doc/' + env.doc2path(env.docname, base=None)|string %}
-.. raw:: latex
-
-    \nbsphinxstopnotebook{\scriptsize\noindent\strut
-    \textcolor{gray}{\dotfill\ \sphinxcode{\sphinxupquote{\strut
-    {{ docname | escape_latex }}}} ends here.}}
-"""
-
-mathjax3_config = {
-    'tex': {'tags': 'ams', 'useLabelIds': True},
-}
-
-
-# See https://www.sphinx-doc.org/en/master/latex.html
-latex_elements = {
-    'papersize': 'a4paper',
-    'printindex': '',
-    'sphinxsetup': r"""
-HeaderFamily=\rmfamily\bfseries,
-div.note_border-TeXcolor={HTML}{E0E0E0},
-div.note_border-width=0.5pt,
-div.note_box-decoration-break=slice,
-div.warning_border-TeXcolor={HTML}{E0E0E0},
-div.warning_border-width=1.5pt,
-div.warning_background-TeXcolor={HTML}{FBFBFB},
-div.warning_box-decoration-break=slice,
-div.topic_box-shadow=none,
-div.topic_border-TeXcolor={HTML}{E0E0E0},
-div.topic_border-width=0.5pt,
-div.topic_box-decoration-break=slice,
-""",
-    'fontpkg': r"""
-\usepackage{mathpazo}
-\linespread{1.05}  % see http://www.tug.dk/FontCatalogue/urwpalladio/
-\setmainfont{TeX Gyre Pagella}[Numbers=OldStyle]
-\setmonofont{Latin Modern Mono Light}[Numbers=Lining]
-""",
-    'preamble': r"""
-\urlstyle{tt}
-""",
-}
-
-latex_engine = 'lualatex'
-latex_use_xindy = False
-
-latex_table_style = ['booktabs']
-
-latex_documents = [
-    (master_doc, 'nbsphinx.tex', project, author, 'howto'),
-]
 
 latex_show_urls = 'footnote'
 latex_show_pagerefs = True
